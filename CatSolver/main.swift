e39c7ff11151bb7780bcb2f1a705d450d9200c41
Mike Emery <philodoxx@gmail.com>
2014-07-22 22:12:28 -0400

    Initial Commit

---
+++ CatSolver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CatSolver
+//
+//  Created by Michael Emery on 7/22/14.
+//  Copyright (c) 2014 Michael Emery. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
